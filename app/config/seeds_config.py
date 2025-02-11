fd7085ef85b18192a55fc1e2183d6b653f3c19e7
Maula Rifqi Setiawan <maularifqi@gmail.com>
2025-02-11 12:05:20 +0700

    minor update in seed_config

--- app/config/seeds_config.py
+++ app/config/seeds_config.py
@@ -2,12 +2,13 @@ from app.models.users_model import Users
 from app.seeds.user_seeds import user_seeds
 
 seed_configs = {
-        "users": {
-            "model": Users,
-            "data": user_seeds,
-            "fields": ["name_husband", "regional", "phone_number"],
-            "process_function": lambda session, user_seeds: (
-                lambda user: (user.set_phone_number(user_seeds["phone_number"]), user)[1])
-                (Users(name_husband=user_seeds["name_husband"], regional=user_seeds["regional"]))
-        }
+    "users": {
+        "model": Users,
+        "data": user_seeds,
+        "fields": ["name_husband", "regional", "phone_number"],
+        "process_function": lambda session, user_seed: Users(
+            name_husband=user_seed["name_husband"],
+            regional=user_seed["regional"]
+        ).set_phone_number(user_seed["phone_number"])
     }
+}
