c14f3435bddc8de9e4def9dc840620dcd5df6914
asyuri26 <asturijingga@gmail.com>
2025-02-19 18:02:22 +0700

    hadist seeds update

--- app/seeds/hadist_seeds.py
+++ app/seeds/hadist_seeds.py
@@ -1,217 +1,217 @@
 hadist_seeds = [
     {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 1,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 2,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 3,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 4,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 5,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 6,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 7,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 8,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 9,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 10,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 11,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 12,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 13,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 14,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 15,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 16,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 17,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 18,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 19,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 20,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 21,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 22,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 23,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 24,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 25,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 26,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 27,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
         {
         "hadist" : "Reza Oktovian, dikenal dengan mononim YB (sebelumnya dikenal sebagai RAP atau Rapyourbae; lahir 15 Oktober 1987) merupakan personalia YouTube, disjoki, penyanyi-penulis lagu, penyiar langsung, pengusaha, dan aktor berkebangsaan Indonesia.",
-        "kultum" : "https://youtube.com/-cgLdZcJ1h4?si=l_BADRJWPEAA3ptg",
+        "kultum" : "https://youtu.be/-cgLdZcJ1h4?si=0UwPR54idDEhOh4d",
         "day" : 28,
         "tittle" : "REZA ARAP BISA NYANYI ?",
         "channel" : "RezaGamer Idaman"
     },
     {
         "hadist" : "Ignatius Rosoinaya Penyami yang lebih dikenal dengan nama Saykoji atau Igor Saykoji (lahir 8 Juni 1983) adalah seorang rapper, penyanyi dan aktor Indonesia.",
-        "kultum" : "https://youtube.com/XEYkMaxZmIE?si=jShtgkb-RAXHGvA3",
+        "kultum" : "https://youtu.be/XEYkMaxZmIE?si=W1dHYBX-8YBjHJrZ",
         "day" : 29,
         "tittle" : "online mulu",
         "channel" : "Saykocak"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 30,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
     },
     {
         "hadist" : "Rekayasa biomedis (bahasa Inggris: biomedical engineering/BME) adalah pengaplikasian teknik dan prinsip teknik dalam bidang medis, disebut juga teknik biomedis. Bidang ini menggabungkan kemampuan desain dan pemecahan masalah seorang insinyur dengan ilmu medis dan ilmu biologi di bidang kedokteran, seperti diagnosis, pengawasan, dan terapi.",
-        "kultum" : "https://youtube.com/pNTmy3cX5jk?si=_qVh6KlmVQkY0Rw5",
+        "kultum" : "https://youtu.be/pNTmy3cX5jk?si=PfXkq45pr2gE4jHb",
         "day" : 31,
         "tittle" : "PAPI JADI KADEP LAGI ?",
         "channel" : "BME ITS"
